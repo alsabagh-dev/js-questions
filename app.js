ede982027e2b17033be2041179d9488cb242fbc5
Abdelaziz Alsabagh <dev.alsabagh@gmail.com>
2022-01-11 23:36:08 +0200

    add close other questions on open new one

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// traversing the dom
+// // traversing the dom
+/*
 const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach((btn) => {
@@ -6,4 +7,20 @@ btns.forEach((btn) => {
         const question = event.currentTarget.parentElement.parentElement;
         question.classList.toggle('show-text');
     })
+});
+*/
+
+//using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', () => {
+       question.classList.toggle('show-text');
+       questions.forEach((other_question) => {
+        if(other_question !== question) {
+            other_question.classList.remove('show-text');
+        }
+       });
+    });
 });
